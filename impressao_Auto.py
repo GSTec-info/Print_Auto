a8d917e507cd5c70e09b72a09e226465abe30381
Generson Avelino da Silva <generson.avelinosilva@gmail.com>
2022-08-04 08:20:12 -0300

    init

--- impressao_Auto.py
+++ impressao_Auto.py
@@ -18,8 +18,9 @@ dt_e_hr_conv_cmd = date.strftime(dt_e_hr, '%d/%m/%Y as %H:%M:%S') # Convert to B
 make_doc = Document()
 
 # Add Image 'Logo' and Center (Header)
+pathLogoEnterprise = r'path\to\image'
 paragraph1 = make_doc.add_paragraph()
-paragraph1.add_run().add_picture(r'path\to\image', width=Inches(1), height=Inches(0.8))
+paragraph1.add_run().add_picture(pathLogoEnterprise, width=Inches(1), height=Inches(0.8))
 paragraph1.alignment = WD_ALIGN_PARAGRAPH.CENTER
 make_doc.add_heading(' ', 2)
 
@@ -27,25 +28,29 @@ make_doc.add_heading(' ', 2)
 make_doc.add_heading('Auto Report', 0).alignment = WD_ALIGN_PARAGRAPH.CENTER
 
 # Document Body
+pathImageColor = r'path\to\image' # Path to image
 make_doc.add_heading(f'************ TEST ************', 1).alignment = WD_ALIGN_PARAGRAPH.CENTER
 make_doc.add_heading(f'Generated at: {dt_e_hr_conv}', 1).alignment = WD_ALIGN_PARAGRAPH.CENTER
 make_doc.add_heading(' ', 2)
 make_doc.add_heading(' Print Quality: ', 5)
 paragraph2 = make_doc.add_paragraph()
-paragraph2.add_run().add_picture(r'path\to\image', width=Inches(5.5), height=Inches(0.6))
+paragraph2.add_run().add_picture(pathImageColor, width=Inches(5.5), height=Inches(0.6))
 paragraph2.alignment = WD_ALIGN_PARAGRAPH.CENTER
 
 # Save the Document
-make_doc.save(r'path\to\save\archive\doc.docx')
+pathSaveDocument = r'path\to\save\document.docx' # Path to save document
+make_doc.save(pathSaveDocument)
 
 
 
 ############################################## # PRINT ################################################ ##########
+# Variable for path of log file
+pathLog = 'path\\to\\save\\archive\\log.log'
 # Start execution log
-os.system(f"cmd /c echo. >>'path\\to\\log'")
-os.system(f"cmd /c echo ************ TEST ************ >>path\\to\\log'")
-os.system(f"cmd /c echo. >>path\\to\\log'")
-os.system(f"cmd /c echo --- Run on: {dt_e_hr_conv_cmd} >>path\\to\\log'")
+os.system(f"cmd /c echo. >>{pathLog}")
+os.system(f"cmd /c echo ************ TEST ************ >>{pathLog}")
+os.system(f"cmd /c echo. >>{pathLog}")
+os.system(f"cmd /c echo --- Run on: {dt_e_hr_conv_cmd} >>{pathLog}")
 
 # Set the Printer 'IMP-QUALIDADE-COLOR' as default
 printer_list = win32print.EnumPrinters(2)
@@ -53,12 +58,12 @@ printer = printer_list[0]
 win32print.SetDefaultPrinter(printer[2])
 print(printer[2])
 
-os.system(f"cmd /c echo --- Received printer: {printer[2]} >>path\\to\\log'")
+os.system(f"cmd /c echo --- Received printer: {printer[2]} >>{pathLog}")
 
 # Set the path of the print folder and the file to be printed
 path = r"path\to\archive"
 file = r"name\to\archive.docx"
-os.system(f"cmd /c echo --- Printed Document: {file} >>path\\to\\log'")
+os.system(f"cmd /c echo --- Printed Document: {file} >>{pathLog}")
 
 # Print the file
 win32api.ShellExecute(0, "print", file, None, path, 0)
@@ -71,7 +76,7 @@ win32print.SetDefaultPrinter(OldPrinter[2])
 print(OldPrinter[2])
 
 # End the script
-os.system(f"cmd /c echo. >>path\\to\\log'")
-os.system(f"cmd /c echo --------------------End------------------- - >>path\\to\\log'")
+os.system(f"cmd /c echo. >>{pathLog}")
+os.system(f"cmd /c echo --------------------End------------------- - >>{pathLog}")
 
 exit()
